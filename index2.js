eb218f70cbc00166817489ff7e085c244d9f0f02
Radwan Awad <radwan96awad11@gmail.com>
2022-12-07 19:53:54 +0300

    done

---
+++ index2.js
@@ -0,0 +1,23 @@
+alert("welcome to my website my name is radwan")
+let garde = prompt("insert your grade")
+console.log(garde);
+
+
+if(garde>=90){
+console.log("A");
+
+
+}else if (garde>=80){
+
+console.log("B");
+
+}else if (grade>=70){
+console.log("C");
+
+
+}else if (garde > 60){
+console.log(D);
+
+}else if{
+    console.log(F);
+}
